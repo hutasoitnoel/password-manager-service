8ba47e6edb470bdd7b5702930355b83c40c86e75
Noel Anugerah Hutasoit <hutasoitnoel@gmail.com>
2024-05-01 21:21:54 +0700

    update cors config

--- middlewares/handleCors.go
+++ middlewares/handleCors.go
@@ -2,15 +2,18 @@ package middlewares
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/rs/cors"
 )
 
 func HandleCors(router http.Handler) http.Handler {
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		AllowCredentials: true,
+		AllowedOrigins:   []string{os.Getenv("DEVELOPMENT_UI_URL"), os.Getenv("PRODUCTION_UI_URL")},
 		AllowedMethods:   []string{"GET", "POST", "PATCH", "DELETE", "PUT"},
+		AllowedHeaders:   []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+		Debug:            true,
 	})
 
 	return c.Handler(router)
