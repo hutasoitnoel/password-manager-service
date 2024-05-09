471e82c29752645c90335e34f1c400dd147a3c14
Noel Anugerah Hutasoit <hutasoitnoel@gmail.com>
2024-05-09 21:03:38 +0700

    test

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"password-manager-service/controllers"
@@ -17,6 +18,8 @@ func init() {
 
 func main() {
 	router := gin.Default()
+
+	fmt.Println("Test github action")
 
 	// Test endpoints
 	router.GET("/ping", controllers.Ping)
