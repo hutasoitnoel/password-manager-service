bb8b3408b901aef5d1fc5884422698ef19326c25
Noel <hutasoitnoel@gmail.com>
2024-11-11 21:02:09 +0700

    add proxy UI url to cors whitelist

--- middlewares/handleCors.go
+++ middlewares/handleCors.go
@@ -9,7 +9,7 @@ import (
 
 func HandleCors(router http.Handler) http.Handler {
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{os.Getenv("DEVELOPMENT_UI_URL"), os.Getenv("PRODUCTION_UI_URL")},
+		AllowedOrigins:   []string{os.Getenv("DEVELOPMENT_UI_URL"), os.Getenv("PRODUCTION_UI_URL"), os.Getenv("PROXY_UI_URL")},
 		AllowedMethods:   []string{"GET", "POST", "PATCH", "DELETE", "PUT"},
 		AllowedHeaders:   []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
